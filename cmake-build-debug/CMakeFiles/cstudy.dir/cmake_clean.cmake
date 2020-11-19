ae7a0d6acf78c78b46460f7418def0fa192e6b83
nice-creator <lidawei918@gmail.com>
2020-11-20 00:28:52 +0800

    please test c

---
+++ cmake-build-debug/CMakeFiles/cstudy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cstudy.dir/main.c.obj"
+  "cstudy.exe"
+  "cstudy.exe.manifest"
+  "cstudy.pdb"
+  "libcstudy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cstudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
