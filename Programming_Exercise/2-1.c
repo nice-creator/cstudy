0afce9e2ff47c12107e5f73fe383b78d4b12ae8d
nice-creator <lidawei918@gmail.com>
2020-11-27 23:10:08 +0800

    jiayou8

---
+++ Programming_Exercise/2-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("lida wei\n");
+    printf("li\ndawei\n");
+    printf("li");
+    printf("dawei\n");
+    
+    return 0;
+}
---
+++ Programming_Exercise/2-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("lidawei\n");
+    printf("beijingchaoyang\n");
+
+    return 0;
+}
