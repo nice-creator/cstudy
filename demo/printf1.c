c6098180340de12d4f64d258ce8910e54efc6e0e
nice-creator <lidawei918@gmail.com>
2020-12-18 22:29:46 +0800

    2020-12-18

---
+++ demo/printf1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a1 = 20, a2 =345,a3 = 700,a4 =2222;
+    int b1 = 323,b2 =2324,b3 = 3123312,b4 = 122;
+    int c1 = 111,c2 = 22312,c3 = 123123,c4 = 12456;
+
+    printf("%-9d%-9d%-9d%-9d\n",a1,a2,a3,a4);
+    printf("%-9d%-9d%-9d%-9d\n",b1,b2,b3,b4);
+    printf("%-9d%-9d%-9d%-9d\n",c1,c2,c3,c4);
+
+    return 0; 
+}
