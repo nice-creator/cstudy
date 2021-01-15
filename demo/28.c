f6c34b2de45e109b580a3df2b97beb8bc6be160f
nice-creator <lidawei918@gmail.com>
2021-01-15 17:58:28 +0800

    2021-1-15

--- demo/28.c
+++ demo/28.c
@@ -1,48 +1,42 @@
-#include <stdio.h>
-#include <string.h>
-
-void tongji(char a[], int b, int c, int d, int e)
+#include<stdio.h>
+#include<string.h>
+void tongji(char a[111])
 {
-    int len = 0;
+    int len=0;
     len = strlen(a);
-    b = 0;
-    c = 0;
-    d = 0;
-    e = 0;
-    for(int i=0;i<len;i++)
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+    int i = 0;
+
+    for(i;i<len;i++)
     {
-        if ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
+        if((a[i]>='a' && a[i]<='z')||(a[i]>='A' && a[i] <= 'z'))
             {
                 b++;
             }
-         else if (a[i] >= '0' && a[i] <= '9')
-             {
+        else if (a[i]>='0' && a[i] <='9')
+            {
                 c++;
             }
-         else if (a[i] == ' ')
-             {
+        else if (a[i]==' ')
+            {
                 d++;
-             }
-         else
-              {
-                e++;           
-             }
+            }
+        else
+            {
+                e++;
+            }
     }
+    printf("%d %d %d %d \n", b, c, d, e);
+
 }
+
 int main()
-{
-    int b,c,d,e;
-    char a[10000];
-    scanf("%[^\n]",a)
-    tongji(a,b,c,d,e);
-    printf("%d%d%d%d",b,c,d,e);
+{   
+    char a[111];
+    scanf("%s",&a);
+    tongji(a);
     return 0;
 }
-
-
-
-
-
-
-
-
