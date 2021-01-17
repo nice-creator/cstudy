000b5f7dece6820807b9646a27ab7970265c5edf
lidawei <lidawei918@gmail.com>
2021-01-17 13:06:25 +0800

    2021-1-17

--- demo/31.c
+++ demo/31.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char str[100]="hello";
-    int i=0;
-    // 逐个字符遍历
-    for(i=0;i<6;i++)
-    {
-        printf("%c",str[i]);
-    }
-    printf("\n");
-    
-    // 直接整体遍历调用数组
-    printf("%s\n",str);
-        
+    char str1[]={'h','e','l','l','o'}; 
+    char str2[]="hello";
+    printf("%d\n",sizeof(str1));  //5
+    printf("%d\n",sizeof(str2));  //6
+    // 定义字符数组的时候，如果没有定义大小
+    // 采用逐个字符定义不会再末尾补\0，空位符
     return 0;
 }
